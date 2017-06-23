7a52864072dc8c3e2759a8bf414533b30e40bf2b
yy2692 <yy2692@tc.columbia.edu>
2017-06-22 21:10:03 -0400

    everything

--- main.js
+++ main.js
@@ -33,20 +33,23 @@ function logIn() {
     return;
   }
   var i;
-  for (i > 0; i < userData.length; i++) {
+  for (i = 0; i < userData.length; i++) {
     if (userData[i].name === userNameEl.value) {
       if (userData[i].password === passWordEl.value) {
-        alert("Dear "+userNameEl.value+",your journey just begin!");
+        alert("Dear " + userNameEl.value + ",your journey just begin!");
+        document.querySelector('#logInForm').innerHTML = '<h2 class="text-center">Welcome!</h2>';
+        enableNavBar();
+        return;
       } else {
         alert("wrong password");
-        return;
-      } }else {
-        alert("no username found");
+        resetInput();
         return;
       }
+    }
   }
-  document.querySelector('#logInForm').innerHTML='<h2 class="text-center">Welcome!</h2>';
-  enableNavBar();
+  alert("no username found");
+  resetInput();
+  return;
 }
 
 function validateTask(task) {
@@ -61,25 +64,36 @@ function resetInput() {
   userNameEl.value = "";
   passWordEl.value = "";
 }
-// function enableNavBar() {
-//   document.querySelector('#nav2').classList.remove("disabled");
-//   document.querySelector('#nav3').classList.remove("disabled");
-//   document.querySelector('#nav4').classList.remove("disabled");
-//   document.querySelector('#nav5').classList.remove("disabled");
-// } this is fancy code, but I'll also need to change the link of the navs, so I used innerHTML in the end.
 
 function enableNavBar() {
   var a;
-  a = '<br><li id="nav1"role="presentation" class="active"><a href="#">Get Started</a></li><br><li id="nav2" role="presentation" class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="colorwheel.html" role="button" aria-haspopup="true" aria-expanded="false">Color Wheel <span class="caret"></span></a><ul class="dropdown-menu"><li id="nav3"><a href="colorwheel.html">Primary Color</a></li><li id="nav4"><a href="colorwheel.html">Complementary Color</a></li></ul></li><br><li id="nav5" role="presentation" ><a href="colorcontext.html">Color Context</a></li>';
+  a = '<br><li role="presentation" class="active"><a href="#">Get Started</a></li><br><li role="presentation" class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="colorwheel.html" role="button" aria-haspopup="true" aria-expanded="false">Color Wheel <span class="caret"></span></a><ul class="dropdown-menu"><li><a href="colorwheel.html">Color Wheel</a></li><li><a href="colorwheel.html#primary-color">Primary Color</a></li><li><a href="colorwheel.html#complementary-color">Complementary Color</a></li><li><a href="colorwheel.html#quiz">Quiz</a></li></ul></li><br><li role="presentation"><a href="colorcontext.html">Color Context</a></li>';
   document.querySelector('#nav').innerHTML = a;
 }
 
 function getTimeStamp() {
   return Date.now(); // this returns a timestamp in milliseconds since 1970.
 }
+
 //colorwheel page
-function updateHomePage(){
-  document.querySelector('#logInForm').innerHTML='<h2 class="text-center">Welcome!</h2>';
-  enableNavBar();
-}
+
 //colorcontext page
+function smallPractice() {
+  document.querySelector('#colorContextIntro').classList.add("hidden");
+  document.querySelector('#smallPractice').classList.remove("hidden");
+  document.querySelector('#paintSquares').classList.remove("hidden");
+}
+
+function paintSquare() {
+  document.querySelector('#squares').classList.remove("hidden");
+  document.querySelector('#paintRed').classList.remove("hidden");
+}
+
+function paintRed() {
+  var squares = document.querySelectorAll('.small-square');
+  var i;
+  for (i = 0; i < 4; i++) {
+    squares[i].className = "red-square";
+    document.querySelector('#paintBigSquares').classList.remove("hidden");
+  }
+} //small bug here
