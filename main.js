1baade84fbb74ad05df6a86fca1e93cbb5762c0f
yy2692 <yy2692@tc.columbia.edu>
2017-06-26 17:14:10 -0400

    final change

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ function logIn() {
     return;
   }
   var i;
-  for (i = 0; i < userData.length; i++) {
+  for (var i = 0; i < userData.length; i++) {
     if (userData[i].name === userNameEl.value) {
       if (userData[i].password === passWordEl.value) {
         alert("Dear " + userNameEl.value + ",your journey just begin!");
@@ -72,12 +72,103 @@ function enableNavBar() {
 }
 
 function getTimeStamp() {
-  return Date.now(); // this returns a timestamp in milliseconds since 1970.
+  return Date.now();
 }
 
 //colorwheel page
 
+var quizData = [{
+    id: "q1",
+    question: "What is this called?",
+    answer: "color wheel"
+  },
+  {
+    id: "q2",
+    question: "What is this called?",
+    answer: "tertiary colors"
+  },
+  {
+    id: "q3",
+    question: "What is red + blue?",
+    answer: "purple"
+  },
+  {
+    id: "q4",
+    question: "What is yellow + blue?",
+    answer: "green"
+  },
+  {
+    id: "q5",
+    question: "What is the complementary color of the color on the right?",
+    answer: "purple"
+  },
+  {
+    id: "q6",
+    question: "What is the complementary color of the color on the right?",
+    answer: "red-orange"
+  }
+]
+
+function quizStart() {
+  document.querySelector('#color-wheel').classList.add("hidden");
+  document.querySelector('#primary-color').classList.add("hidden");
+  document.querySelector('#complementary-color').classList.add("hidden");
+  document.querySelector('#quizQuestions').classList.remove("hidden");
+}
+
+function getAnswer(q) {
+  for (var i = 0; i < 4; i++) {
+    if (document.getElementsByName(q)[i].checked) {
+      return document.getElementsByName(q)[i].value
+    }
+  }
+  return false;
+}
+
+function collectAnswers() {
+  var a1 = getAnswer("q1");
+  var a2 = getAnswer("q2");
+  var a3 = getAnswer("q3");
+  var a4 = getAnswer("q4");
+  var a5 = getAnswer("q5");
+  var a6 = getAnswer("q6");
+  return [a1, a2, a3, a4, a5, a6];
+}
+
+function checkAnswer(i) {
+  var answer = collectAnswers();
+  if (quizData[i].answer === answer[i]) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function submitQuiz() {
+  var answer = collectAnswers();
+  for (var i = 0; i < 6; i++) {
+    if (answer[i] === false) {
+      alert("please finish all questions!");
+      return;
+    }
+  }
+  var html = "";
+  for (var i = 0; i < 6; i++) {
+    html += `<p>Question ${i}: ${checkAnswer(i)}</p>`;
+  }
+  document.querySelector("#quizResult").innerHTML = "<h3 class='text-black'>Quiz Result</h3>" + html;
+  document.querySelector('#goBack').classList.remove("hidden");
+}
+
+function goBack(){
+  document.querySelector('#color-wheel').classList.remove("hidden");
+  document.querySelector('#primary-color').classList.remove("hidden");
+  document.querySelector('#complementary-color').classList.remove("hidden");
+  document.querySelector('#quizQuestions').classList.add("hidden");
+}
+
 //colorcontext page
+
 function smallPractice() {
   document.querySelector('#colorContextIntro').classList.add("hidden");
   document.querySelector('#smallPractice').classList.remove("hidden");
@@ -92,8 +183,28 @@ function paintSquare() {
 function paintRed() {
   var squares = document.querySelectorAll('.small-square');
   var i;
-  for (i = 0; i < 4; i++) {
+  for (var i = 0; i < 4; i++) {
     squares[i].className = "red-square";
     document.querySelector('#paintBigSquares').classList.remove("hidden");
   }
-} //small bug here
+}
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("color", ev.target.classList);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var list = ev.dataTransfer.getData("color");
+  var data = list.replace("color-box ", "");
+  ev.currentTarget.className = `big-square ${data}`;
+}
+
+function difference() {
+  document.querySelector('#paintRed').classList.add("hidden");
+  document.querySelector('#difference').classList.remove("hidden");
+}
